ff04d1d16f591b29e9196556f6be82a634aef99d
cruelwalker <42969080+cruelwalker@users.noreply.github.com>
2019-11-27 07:49:06 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once('user_controller.php');
 require_once('user_model.php');
 
@@ -7,25 +8,23 @@ require_once('user_model.php');
 $user_controller = new UserController();
 
 switch ($login) {
-	case 'Login':
-		//we validate username and password
-		$username = $_POST['username'];
-		$password = $_POST['password'];
+    case 'Login':
+        //we validate username and password
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-	if ($user_controller->login($username, $password)) {
-		header('Location:main.php');
-	}else{
-		header("Location:login.php?err=1");
-	}
-	break;
-	case 'logout':
-		$user_controller->logout();
-		header('Location:login.php');
-		break;
-	default:
-		header('Location:login.php');
-		break;
+        if ($user_controller->login($username, $password)) {
+            header('Location:main.php');
+        } else {
+            header("Location:login.php?err=1");
+        }
+        break;
+    case 'logout':
+        $user_controller->logout();
+        header('Location:login.php');
+        break;
+    default:
+        header('Location:login.php');
+        break;
 }
-
-
 ?>
